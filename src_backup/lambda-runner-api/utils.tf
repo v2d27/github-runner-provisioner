bf6323fa6a93d163add0c9acea5f30320f3484ce
Hercules <ducduc08@gmail.com>
2026-09-15 21:08:39 +0700

    Remove old code pattern

--- src_backup/lambda-runner-api/utils.tf
+++
@@ -1,5 +0,0 @@
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/lambda_function"
-  output_path = "${path.module}/lambda_function.zip"
-}
--- src_backup/lambda-runner-worker/Dockerbuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-AWS_ECR_URI=$1
-
-docker build --platform linux/amd64 -f ./Dockerfile.lambda  -t docker-image:test .
-
-docker tag docker-image:test $AWS_ECR_URI
-
-docker push $AWS_ECR_URI
